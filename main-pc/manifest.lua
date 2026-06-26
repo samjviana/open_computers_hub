cd6701e8fff375862789950d0040388a265c3239
samjviana <sam.jviana@gmail.com>
2026-06-26 16:19:29 -0300

    [Main-PC]{feat} added manifest which controls what files will be downloaded

---
+++ main-pc/manifest.lua
@@ -0,0 +1,13 @@
+return {
+  "main.lua",
+
+  "lib/config.lua",
+  "lib/components.lua",
+  "lib/util.lua",
+
+  "lib/tasks/cows.lua",
+  "lib/tasks/repair.lua",
+  "lib/tasks/overflow.lua",
+
+  "lib/ui/storage_display.lua"
+}
